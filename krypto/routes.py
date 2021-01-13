9e744302359a70297d8194cd17854ea0f91e5256
carlos sanchez-valero <carlossanchez-valero@MacBook-Pro-de-carlos.local>
2021-01-13 22:05:02 +0100

    Version 2. Paginas creadas y mas o menos colocadas

--- krypto/routes.py
+++ krypto/routes.py
@@ -1,13 +1,25 @@
 from krypto import app
-from flask import render_template
+from flask import render_template, request, url_for, redirect
+import sqlite3
+from datetime import date, time
 
-
+DBfile = 'krypto/data/database.db'
 
 
 @app.route('/')
 def movements_table ():
+    
+    conn = sqlite3.connect (DBfile)
+    c = conn.cursor()
 
-    return render_template ('movement_table.html')
+    c.execute ('SELECT date, time, from_currency, from_quantity, to_currency, to_quantity FROM movements;')
+    
+    invested = c.fetchall()
+
+
+    conn.close()
+    
+    return render_template ('movement_table.html', datos=invested)
 
 @app.route ('/purchase')
 def purchase ():
