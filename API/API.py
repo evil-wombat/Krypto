c574b0e5727694fac82b124572872f6c087a9951
carlos sanchez-valero <carlossanchez-valero@MacBook-Pro-de-carlos.local>
2021-01-19 13:02:33 +0100

    Activada la comunicacion entre app y bbdd. Se realizan GET/POST

--- API/API.py
+++ API/API.py
@@ -3,7 +3,7 @@ from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
 
-def API_Name (param):
+def Name (param):
   url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
   parameters = {
     'start':'1',
@@ -33,9 +33,7 @@ def API_Name (param):
   except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
   
-
-
-def API_Price (param):
+def Price (param):
   
   url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
   parameters = {
@@ -61,5 +59,59 @@ def API_Price (param):
   except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
 
-symbols = API_Name ('symbol')
-print (symbols [0])
+def Date ():
+  url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+  parameters = {
+    'start':'1',
+    'limit':'12',
+    'convert':'EUR'
+  }
+  headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': '4db76d23-bbcf-4e27-87b0-146f16084535',
+  }
+
+  session = Session()
+  session.headers.update(headers)
+
+  try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    
+    timestamp = (data ['status']['timestamp'])
+
+    date = timestamp [0:10]
+    
+    return date
+
+  except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
+
+def Time ():
+  url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+  parameters = {
+    'start':'1',
+    'limit':'12',
+    'convert':'EUR'
+  }
+  headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': '4db76d23-bbcf-4e27-87b0-146f16084535',
+  }
+
+  session = Session()
+  session.headers.update(headers)
+
+  try:
+    response = session.get(url, params=parameters)
+    data = json.loads(response.text)
+    
+    timestamp = (data ['status']['timestamp'])
+
+    time = timestamp [11:19]
+    
+    return time
+
+  except (ConnectionError, Timeout, TooManyRedirects) as e:
+    print(e)
+  
--- krypto/routes.py
+++ krypto/routes.py
@@ -1,11 +1,9 @@
 from krypto import app
 from flask import render_template, request, url_for, redirect
 import sqlite3
-from datetime import date, time
-from API import main
+from API import API
 
 DBfile = 'krypto/data/database.db'
-
 
 @app.route('/')
 def movements_table ():
@@ -21,15 +19,47 @@ def movements_table ():
     
     return render_template ('movement_table.html', datos=movements)
 
-@app.route ('/purchase')
+@app.route ('/purchase', methods = ['GET', 'POST'])
 def purchase ():
 
-    symbols = main.API_Name ('symbol')
+    if request.method == 'POST':
 
-    return render_template ('purchase.html', symbols = symbols)
+        conn = sqlite3.connect (DBfile)
+        c = conn.cursor()
+
+        c.execute ('INSERT INTO movements (date, time, from_currency, from_quantity, to_currency, to_quantity) VALUES (?, ?, ?, ?, ?, ?);', 
+            (
+                API.Date (),
+                API.Time (),
+                request.form.get('Coins_from'),
+                request.form.get('Q_from'),
+                request.form.get('Coins_to'),
+                request.form.get('Q_to')
+            )
+        )
+
+        conn.commit ()
+        conn.close ()
+    
+        return redirect (url_for('movements_table'))
+    
+    return render_template ('purchase.html')
 
 
 @app.route ('/status')
 def status ():
 
-    return render_template ('status.html')
+    conn = sqlite3.connect (DBfile)
+    c = conn.cursor()
+
+    c.execute ('SELECT date, time, from_currency, from_quantity, to_currency, to_quantity FROM movements;')
+    
+    movements = c.fetchall()
+
+    conn.close()
+
+    invested = 0
+    for cantidad in movements:
+        invested += cantidad [3]
+
+    return render_template ('status.html', total = invested)
